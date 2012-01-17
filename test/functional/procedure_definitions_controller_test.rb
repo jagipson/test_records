64b3e0f9cfe90ed94ad218e313503870c38936cc
Jeffrey Gipson <jeffagipson@gmail.com>
2012-01-17 09:38:56 -0600

    passing tests for procedure_definitions deletion

--- test/functional/procedure_definitions_controller_test.rb
+++ test/functional/procedure_definitions_controller_test.rb
@@ -29,23 +29,43 @@ class ProcedureDefinitionsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should not get edit" do
-    assert_raise AbstractController::ActionNotFound do
-      get :edit, id: @procedure_definition.to_param
-    end
+  test "update procedure_definition should add record and associate" do
+    count_before = ProcedureDefinition.all.count
+    @procedure_definition.attributes[:name] = "oneB"
+    put :update, id: @procedure_definition.to_param, record: @procedure_definition.attributes
+    assert_response 302, "error updating record"
+    count_after = ProcedureDefinition.all.count
+    assert count_before + 1 == count_after, "record count did not increment by 1"
+    @p_one = ProcedureDefinition.find(@procedure_definition.id)
+    @p_one_b = @p_one.new_version
+    assert @p_one.id != @p_one_b.id, "did not create distinct record"
+
   end
 
-  test "should not update procedure_definition" do
-    assert_raise ActionView::Template::Error do
-      put :update, id: @procedure_definition.to_param, record: @procedure_definition.attributes
-    end
-  end
-
-  test "should destroy procedure_definition" do
+  test "should destroy procedure_definition if newest" do
     assert_difference('ProcedureDefinition.count', -1) do
       delete :destroy, id: @procedure_definition.to_param
     end
-
     assert_redirected_to procedure_definitions_path
+  end
+
+  test "should not destroy procedure_definition if exist newer" do
+    # Use fixture :two - it has a child and grand_child (two newer versions)
+    @p_two = procedure_definitions(:two)
+    # Prove that we got the record okay
+    assert ProcedureDefinition.exists?(@p_two), "fixture two destroyed before deleted"
+    # prove that deleting it at this point does not work (subject still
+    # exists)
+    delete :destroy, id: @p_two.id
+    assert ProcedureDefinition.exists?(@p_two), "fixture two was deleted when it had a newer version"
+
+    # delete grandchild, child, and subject
+    delete :destroy, id: @p_two.new_version.new_version.id
+    delete :destroy, id: @p_two.new_version.id
+    delete :destroy, id: @p_two.id
+
+    # test that subject no longer exists.
+    assert ! ProcedureDefinition.exists?(@p_two), "fixture two was not deleted after deleting the newer version"
+    
   end
 end
